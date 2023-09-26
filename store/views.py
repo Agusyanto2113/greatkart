2cff63569b6b420638ef2972bb54e7288230289d
Agusyanto2113 <agusyanto2113@gmail.com>
2023-09-26 17:03:59 +0700

    Variations Early Step

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from carts.models import CartItem
 from category.models import Category
 from store.models import Product
 from carts.views import _cart_id
-
+from django.db.models import Q
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 from django.http import HttpResponse
@@ -46,3 +46,16 @@ def product_detail(request,category_slug,product_slug):
     }
       
     return render(request, 'store/product_detail.html',context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products':products,
+        'product_count':product_count,
+    }
+    return render(request,'store/store.html',context)
